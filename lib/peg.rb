a7604c8cb9a65628e9a875947d5335774cac68fa
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-10 18:12:57 -0800

    Add class to file

--- lib/peg.rb
+++ lib/peg.rb
@@ -1,1 +1,9 @@
-This is peg
+class Peg
+  attr_reader :color,
+              :position
+
+  def initialize(color, position)
+    @color = color #r or R has to work
+    @position = position
+  end
+end
