f40bc2629f96a59ffb5c6386691e513c2dd5982f
aferencz1987 <alex.ferencz6@gmail.com>
2021-02-11 17:15:43 -0700

    Create comparison method in turn.rb and add welcome message to messages.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,4 +3,20 @@ class Turn
   def initialize
 
   end
+
+  def main_game
+    puts "hey bud, type in some stuff"
+    print "> "
+    user = gets.chomp
+    computer = ["r", "b", "g", "y"]
+
+      if
+        user[0] == computer[0] && user[1] == computer[1] && user[2] == computer[2] && user[3] == computer[3]
+        puts "Congratulations! You guessed the sequence '#{computer}' in 8 guesses over #{time}.
+              Do you want to (p)lay again or (q)uit?"
+      else
+        puts "nope, try again"
+      end
+
+
 end
