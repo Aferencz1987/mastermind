5422cb015a5c0fd93b53fc26b7a7c37b5fa76512
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-13 08:50:24 -0800

    Add methods

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,24 +1,65 @@
-puts "Welcome to MASTERMIND
+class Messages
+  attr_reader :beginning_message,
+              :instruct,
+              :error
 
-I have generated a beginner sequence with four elements made up of: (r)ed,
-(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
-What's your guess?"
 
-print "> "
-start = $stdin.gets.chomp
+  def initialize
+    @beginning_message = beginning_message
+    @instruct = instruct
+    @error = error
+  end
 
-if start == "p"
-  puts "Okay, let's DO THIS!"
+  def welcome
+    @beginning_message = puts "Welcome to MASTERMIND
 
-  print "> "
-  play = $stdin.gets.chomp
+    Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
-elsif start == "i"
-  puts "Watch the youtube video, yo."
+    print "> "
+    start = gets.chomp
 
-  print "> "
-  instructions = $stdin.gets.chomp
+    if start == "p"
+      #This is where TURN lives
+      #
+      puts "Okay, let's DO THIS!"
 
-else start == "q" 
-  puts "Well, fine then.  We didn't want to play with you either."
+      print "> "
+      play = gets.chomp
+
+    elsif start == "i"
+      puts "Watch the youtube video, yo."
+
+      print "> "
+      instructions = gets.chomp
+
+    else start == "q"
+      puts "Well, fine then.  We didn't want to play with you either."
+    end
+  end
+
+  def instructions
+    @instruct = puts "The object of MASTERMIND is to guess a secret code created by the computer consisting of a series of 4 colors.
+You may choose any combination of (r)ed, (y)ellow, (g)reen, or (b)lue.
+You may have any color repeat as many times as you’d like until a series of 4 is reached.
+You will be provided feedback per attempt. Feedback will look as follows:
+
+  “'RRGB' has 3 of the correct colors with 2 in the correct positions You've taken 1 guess”
+
+You may press ‘q’ for quit at any time. Happy guessing!"
+  end
+
+  def error_message
+    @error = puts "Hey friend, I noticed you have the wrong amount of elements. Try putting a total of 4 in."
+  end
 end
+
+message = Messages.new
+# message.welcome
+# message.instructions
+message.error_message
+
+#Have a method called begin or start have it generate code and print out welcome message.
+
+
+
+#We need have the code generator method here.
