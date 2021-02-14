e4bdf1fc407376efcfe07986ae82106e79bcb689
aferencz1987 <alex.ferencz6@gmail.com>
2021-02-13 23:21:24 -0700

    Add finished cheat method and computer code method.

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game.rb'
 
+
 class GameTest < Minitest::Test
 
   def test_it_exists
@@ -11,8 +12,13 @@ class GameTest < Minitest::Test
 
   def test_is_has_a_secret_code
     game = Game.new
-    expected = true
+    expected = game.compcode
+    assert_equal expected, game.compcode
+  end
 
-    assert_equal expected, game.secret_code
+  def test_can_cheat
+    game = Game.new
+    expected = @compcode
+    assert_equal expected, game.cheat
   end
 end
