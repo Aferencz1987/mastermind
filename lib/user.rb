e58c0bb5b7e18bee0f79008699fd957a4dc29cd8
aferencz1987 <alex.ferencz6@gmail.com>
2021-02-13 23:18:45 -0700

    Add length error test and method.

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,21 @@
 class User
-  
+  attr_reader :user
+  def initialize(user)
+    @user = user
+
+  end
+
+  def length_error?
+    if @user.length == 4
+      false
+    else
+      true
+    end
+    # if @user.count > 4 || @user.count < 4
+    #   @erroneous_length = true
+    # end
+  end
+
+
+
 end
