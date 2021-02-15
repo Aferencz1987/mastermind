b96e9ed2e10a0e164109b071080a660f73274e1b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-14 20:20:50 -0800

    Finish compare

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,33 @@
 
 class Game
   attr_reader :code_options,
-              :compcode
+              :compcode,
+              :guesses
   def initialize
-    @code_options = ['r', 'b', 'g', 'y']
-    @compcode = @code_options.shuffle
+    @code_options = ["rbyg"]
+    @compcode = ["r", "b", "b", "b"]#don't forget to CHANGE ME BACK, bitch
+    @guesses = ["r", "b", "b", "y"]#guesses.split
   end
 
-  def cheat
-    print @compcode
+
+  def color_counter
+    color_count = []
+    4.times do |index|
+      if @guesses[index] == @compcode[index]
+        color_count << 1
+      end
+    end
+    color_count.sum
   end
 
+# Do we still need this?  I feel like we do.
+  def position_counter
+    position_count = []
+    4.times do |index|
+      if @guesses[index] == @compcode[index]
+        position_count << 1
+      end
+    end
+    position_count.sum
+  end
 end
