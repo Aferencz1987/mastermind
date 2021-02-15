8242977dd58f0c24992d2253c0583eedc87f94c9
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-14 21:54:12 -0800

    Tweek color_counter method

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,33 @@
 
 class Game
-  attr_reader :code_options,
-              :compcode,
-              :guesses
-  def initialize
-    @code_options = ["rbyg"]
-    @compcode = ["r", "b", "b", "b"]#don't forget to CHANGE ME BACK, bitch
-    @guesses = ["r", "b", "b", "y"]#guesses.split
-  end
+  attr_accessor :compcode,
+                :guesses
 
+
+
+  def initialize
+    @compcode = compcode
+    @guesses = guesses
+  end
 
   def color_counter
     color_count = []
-    4.times do |index|
-      if @guesses[index] == @compcode[index]
+    4.times do |guess|
+      if @guesses[guess] == @compcode[guess]
         color_count << 1
       end
     end
     color_count.sum
   end
 
-# Do we still need this?  I feel like we do.
-  def position_counter
-    position_count = []
-    4.times do |index|
-      if @guesses[index] == @compcode[index]
-        position_count << 1
-      end
-    end
-    position_count.sum
-  end
+
+  # def position_counter
+  #   position_count = []
+  #   4.times do |index|
+  #     if @guesses[index] == @compcode[index]
+  #       position_count << 1
+  #     end
+  #   end
+  # end
+  # position_count.sum
 end
