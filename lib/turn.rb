47deeef6842cb1fcc244d0652fdd3adbc1fd28dd
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-02-14 21:56:26 -0800

    Add length_error method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,40 +1,20 @@
 class Turn
 
   def initialize
-
+    @user = user
   end
 
-  # def length_error?
-  #   require "pry"; binding.pry
-  #
-  #   if user.count > 4 || user.count < 4
-  #     print "crap"#error_message.messages
-  #   else
-  #     true
-  #   end
-  # end
+  def length_error?
+    if @user.length == 4
+      false
+    else
+      true
+      puts message.error_message
+    end
+  end
 
 end
 
 
 
 @compcode
-
-
-
-  # def main_game
-  #   puts "hey bud, type in some stuff"
-  #   print "> "
-  #   user = gets.chomp
-  #   computer = ["r", "b", "g", "y"]
-  #
-  #     if
-  #       user[0] == computer[0] && user[1] == computer[1] && user[2] == computer[2] && user[3] == computer[3]
-  #       puts "Congratulations! You guessed the sequence '#{computer}' in 8 guesses over #{time}.
-  #             Do you want to (p)lay again or (q)uit?"
-  #     else
-  #       puts "nope, try again"
-  #     end
-
-
-#Should be able to have quit method at any point
